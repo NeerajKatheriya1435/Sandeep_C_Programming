ca807d4f29e8c51df3fd942b6431ada114fd4a55
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-08-07 12:05:39 +0530

    initial sandeep classes

---
+++ day1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is My First Program\n");
+    printf("Subham Kumar");
+    return 0;
+}
---
+++ day2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // int a = 7;
+    // int sandeep = 89;
+    // int kumarsanu = 78;
+    // // int 64suman=90;
+    // int sandeep12 = 78;
+    // int suman_kumar = 89;
+    // int num2 = 456;
+    // int forAnoop=67;
+    // printf("%d", sandeep + num2);
+
+    // Today we learned about varibles and data types
+
+    // int age = 45;
+    // float salary = 20000.67;
+    // double weigtht = 78.688655;
+    // char myFav = 'g';
+
+    // printf("My age is %d\n", age);
+    // printf("My salary is %.1f\n", salary);
+    // printf("My weigtht is %f\n", weigtht);
+    // printf("My Favorite Character is %c\n", myFav);
+    // int a = 8;
+    // printf("The number is:%d", a);
+    // printf("Hello hii \nkaise ho");
+
+    int num1 = 15;
+    int num2 = 5;
+    printf("The sum is: %d\n", num1 + num2);         // 20
+    printf("The subtraction is: %d\n", num1 - num2); // 10
+    printf("The multiply is: %d\n", num1 * num2);    // 75
+    printf("The divide is: %d\n", num1 / num2);      // 3
+    printf("The mmodulous is: %d\n", num1 % num2);   // 0
+
+    return 0;
+}
