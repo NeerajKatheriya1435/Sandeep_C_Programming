dc56304d69ccdea54b01a2388ebad17ed76cc9fc
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-08-11 12:29:00 +0530

    Operators in c

---
+++ day3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 78;
+    int num2 = 78;
+    int sandeep = 34;
+
+    // Comparison operators
+
+    // printf("%d\n", (num1 == num2));    // true - 1
+    // printf("%d\n", (num1 == sandeep)); // false - 0
+    // printf("%d\n", (num1 > sandeep));  // true -1
+    // printf("%d\n", (num1 >= num2));    // true -1
+    // printf("%d\n", (num1 < sandeep));  // false - 0
+    // printf("%d\n", (num2 != sandeep)); // false - 0
+    // int b=78;
+
+    // Logical Operators
+
+    // int ayushKumar = 67;
+    // printf("%d\n", (num1 > num2 && num1 > sandeep && num2 >= ayushKumar));  // 0
+    // printf("%d\n", !(num1 > num2 || num1 > sandeep || num2 >= ayushKumar)); // 1
+
+    // Assingment operators
+
+    // assign value tp shivamVar
+    // int shivamVar = 5;
+    // shivamVar = 16; // update value
+    // shivamVar = 78; // update value
+    // shivamVar = 7;  // update value //7
+    // shivamVar += 9; // 16
+    // shivamVar -= 4; // 12
+    // shivamVar /= 3; // 4
+    // shivamVar *= 5; // 20
+    // shivamVar %= 6; // 20
+    // printf("%d", shivamVar);
+
+    // Increment / Decrement Operaators
+
+    int a = 7; // 7
+    // a = a + 1; // 8
+    // ++a; // a++;
+
+    // printf("%d", ++a);
+    // printf("%d\n", a--);
+    printf("%d\n", --a);
+    printf("%d\n", a);
+
+    return 0;
+}
