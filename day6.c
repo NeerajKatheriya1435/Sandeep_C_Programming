705dcd6ee029112d4b6a9ef42bb7aa2383c9b7d9
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-08-13 15:20:48 +0530

    loops concepts

---
+++ day6.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    // int age;
+    // printf("Enter you age\n");
+    // scanf("%d", &age);
+    // if (age <= 0 || age >= 120)
+    // {
+    //     printf("Invalid Age\n");
+    // }
+    // else if (age > 0 && age <= 18)
+    // {
+    //     printf("You are child and you can not drive the car\n");
+    // }
+    // else if (age > 80 && age < 120)
+    // {
+    //     printf("You are overAged and you can not drive the car\n");
+    // }
+    // else
+    // {
+    //     printf("You can drive the car\n");
+    // }
+
+    // nested if
+
+    // if (age > 45)
+    // {
+    //     printf("You are graeter the 45\n");
+    //     if (age == 56)
+    //     {
+
+    //         printf("Conguratulations");
+    //     }
+    // }
+
+    printf("%d\n", 1);
+    printf("%d\n", 2);
+    printf("%d\n", 3);
+    printf("%d\n", 4);
+
+    // for loop
+    // for (int i = 0; i < 100000; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    int age;  // declartion
+    age = 89; // asigning the value / initilize value
+
+    int k = 56; // declaration with initilization
+
+    // int a = 0;
+    // while (a < 11)
+    // {
+    //     printf("%d\n", a);
+    //     a++;
+    // }
+
+    return 0;
+}
