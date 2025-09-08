fe3a6394277d49ed158672f3175ce0e719e2bb0d
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-09-08 15:18:07 +0530

    Initial commit for c++

---
+++ C++ Files Sandeep/day1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int suman = 8;
+    // simple print statements
+    int for124_ = 6;
+    // cout << "Hello word";
+    /*
+    bgdfg
+    gfhfgt
+    gfrfthj
+    tfhgtf
+    sdfdgh
+    \rghrfth
+    frhgtfrh
+    Multiline comments
+    */
+    float var1 = 5.78f;
+    cout << var1 << "\n";
+    // returning the output to opearting system with value 0
+
+    double d1 = 8.78994566;
+    cout << d1;
+    char c1 = '&';
+    cout << c1 << "\n"
+         << "Hello\n";
+    bool isPass = false;
+    cout << isPass;
+    return 0;
+}
