ee9a2bbb34577f1ccd163ae83442c198c76a707e
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-09-10 18:20:17 +0530

    refrence variables

--- C++ Files Sandeep/day2.cpp
+++ C++ Files Sandeep/day2.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 
-    int a = 9;
-    int b = 7;
+    // int a = 9;
+    // int b = 7;
     // cout << (a + b) << endl;
     // cout << (a + b) << "\n";
     // cout << (a - b) << endl;
@@ -21,9 +21,23 @@ int main(int argc, char const *argv[])
     // cout << (a > b) << endl;  // 1
     // cout << (a != b) << endl; // 1
 
-    cout << (4 > 6 && 8 > 4 && 3 >= 3) << endl;
+    // cout << (4 > 6 && 8 > 4 && 3 >= 3) << endl;
+    // cout << !(4 > 6 || 8 > 4 || 3 >= 3) << endl;
 
-    cout << !(4 > 6 || 8 > 4 || 3 >= 3) << endl;
+    // inrement /decrement
 
-    return 0;
+    // int num = 7;
+    // cout << num << endl;   // 7
+    // cout << num++ << endl; // 7
+    // cout << num << endl;   // 8
+
+    // int a = (16 > 7) ? 8 : 9;
+    // cout << a;
+
+    // int a = (6, 8, 5);
+
+    // cout << a;
+    // cout << sizeof(a);
+
+        return 0;
 }
---
+++ C++ Files Sandeep/day3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // int age;
+    // cout << "Enter the value of age: " << endl;
+    // cin >> age;
+    // cout << "My age is: " << age << endl;
+
+    // cout << "Hello bhai kaise ho?" << endl
+    //      << "kaha ja rahe ho" << endl;
+    // cout << "Hello sonu" << endl;
+    // double dd = 347.897656;
+    // cout << setprecision(18) << dd;
+    // cout << setw(16) << "hello" << endl;
+    // cout << fixed << setprecision(12) << dd;
+
+    int sandeepAge = 67;
+    int shivam = sandeepAge;
+
+    sandeepAge = 56;
+    shivam = 89;
+    sandeepAge = 23;
+    cout << shivam << endl;
+    cout << sandeepAge << endl;
+    // sandeepAge = 78;
+    // cout << sandeepAge << endl;
+
+    // cout << shivam << endl;     // 78
+    // cout << sandeepAge << endl; // 78
+
+    return 0;
+}
