790e0132817f1eb9a59f99435d80e9fddcbcbc1f
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-19 17:21:05 +0530

    Added files Excel

---
+++ Data_Structures_C++/delete.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        val = data;
+    }
+};
+
+class LinkedList
+{
+};
+
+int main()
+{
+
+    return 0;
+}
