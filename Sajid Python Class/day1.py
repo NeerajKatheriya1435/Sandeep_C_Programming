c6cd51082970a12a3f541dc730f87d1764fc124a
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-21 09:24:16 +0530

    Added Sajid Code

---
+++ Sajid Python Class/day1.py
@@ -0,0 +1,76 @@
+# print("hello")
+# print("hjjnjk")
+
+# //take input from user birthday year and current
+
+# userDateOfBirth=input("Enter number1 : ")
+# userDateOfBirth=input("Enter number2: ")
+
+
+# str1="Suman"
+# str2="Kumari"
+# print(num1+num2)
+# print(str1+" "+str2)
+# int float list tuple dictionary set 
+# num3=8
+# num4=6.78
+# print(num3+num4)
+
+# Typecasting
+
+# implicit typecasting
+# str1=6
+# str1=6.675
+# str1="Suman"
+# str1=True
+# print(str1)
+# print(type(str1))
+# num1=9
+# num2=2.6
+# num3=num1+num2
+# num4=int(num3)
+# print(int(num3))
+# print(type(num4))
+
+# num1=int(input("Enter number1 :"))
+# num2=int(input("Enter number2 :"))
+
+# num3=int(num1)+int(num2)
+# print(num1+num2)
+
+# birthDay=int(input("Enter your birthday"))
+# currentYear=int(input("Enter your currentYear"))
+# print("Your age is: ",(currentYear-birthDay))
+
+list1=["Shivam","Aman","Mahi","Farhan","Rohan","Rohit"]
+
+# str1="Aman-Shubham-Year-rohit"
+# print(len(str1.split("-")))
+# print(shivam)
+# Acess the element
+# print(list1[0])
+# List slicing
+# print(list1[1:4])
+# print(list1[:3])
+# print(list1[1:])
+# print(list1[:])
+# print(list1[-5:-1])
+# print(len(list1))
+
+# update the list
+# list1[2]="Suman"
+# list1.append(999)
+# list1.append("Rohan")
+# print(list1)
+
+# for item in list1:
+#     print(item)
+
+# colors1 = ["voilet", "indigo", "blue", "green"]
+# colors2 = [5,8,2,9,3,8,0,8]
+# colors.sort()
+# colors.reverse()
+# print(colors)
+# print(colors.index(8))
+# print(colors.count(8))
+# print(colors1+colors2)
